a1c92ae8986cfd7c4b5a4cdf38e2d300e6ff34ef
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-03-11 16:55:58 -0300

    inclusão de little avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,12 @@ module ApplicationHelper
 #    end
   end
 
+
+  #little avatar
+  def lavatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=32"
+  end
+
   
 end
